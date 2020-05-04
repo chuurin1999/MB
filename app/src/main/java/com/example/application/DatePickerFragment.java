8b3b36eb45c61358150ce00e26d874de80a8dbce
chuurin1999@gmail.com <b6zh8cdr2ydklyy>
2020-05-05 02:08:11 +0800

    日曆傳回兩位數

--- app/src/main/java/com/example/application/DatePickerFragment.java
+++ app/src/main/java/com/example/application/DatePickerFragment.java
@@ -7,6 +7,7 @@ import android.app.DatePickerDialog;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.DatePicker;
 import java.text.ParsePosition;
@@ -40,7 +41,11 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
-
+        year=Integer.parseInt(twodigits(year));
+        month=Integer.parseInt(twodigits(month));
+        day=Integer.parseInt(twodigits(day));
+        Log.d("月份", String.valueOf(month));
+        Log.d("日號", String.valueOf(day));
         // 建立 DatePickerDialog instance 並回傳
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
@@ -48,9 +53,16 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
     public void onDateSet(DatePicker view, int year, int month, int day)
     {
         Button button = (Button) getActivity().findViewById(vid);
+        Log.d("月份11", String.valueOf(month));
+        Log.d("日號11", String.valueOf(day));
         // 注意 月的起始值是 0，所以要加 1
-        button.setText(""+year+"-"+(month+1)+"-"+day);
+        button.setText(""+twodigits(year)+"-"+twodigits(month+1)+"-"+twodigits(day));
     }
-
+    //把個位數轉二位數，使格式清楚
+    private String twodigits ( int value){
+        double digit2 = Math.floor(value / 10);
+        double digit1 = value % 10;
+        return String.format("%.0f", digit2) + String.format("%.0f", digit1);
+    };
 }
 
